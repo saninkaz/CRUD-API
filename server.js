8b40c444b8a1931f9b90ff5fe8d517b40980bf0c
Sanin <guardbro85@gmail.com>
2024-09-22 13:40:21 +0530

    Project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const app=express()
+
+
+//routes
+
+app.get('/',(req,res)=>{
+    res.send('Hello Welcome to this API')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Node API app is running on port 3000')
+})
